64a2f577afe5727d89687b4941aafcbcc15044ba
liuli <liuli@qq.com>
2022-12-08 23:21:46 +0800

    ProductCard组件开发

---
+++ src/common/constants/productService.ts
@@ -0,0 +1,47 @@
+const MARKETSERVICE_CONFIG = [
+    {
+        marketToolTitle: '课程通',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '操作盘',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '课程通',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '财务中心',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '创意中心',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '意见收藏',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '慕课搜索',
+        marketToolImgUrl: '',
+        link:''
+    },  {
+        marketToolTitle: '大事记',
+        marketToolImgUrl: '',
+        link:''
+    },
+    {
+        marketToolTitle: '营销通',
+        marketToolImgUrl: '',
+        link:''
+    }
+]
+export default MARKETSERVICE_CONFIG;
---
+++ src/pages/index/components/ProductCard/index.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import MARKETSERVICE_CONFIG from 'src/common/constants/productService';
+interface IProps{ }
+interface IStates{}
+export default class ProductCard extends Component<IProps, IStates> {
+    handleServiceItemClick = (url:string) => {
+        window.location.href=url
+    } 
+  render() {
+    return (
+        <div className='product-service-component-box'>
+            <div className='title'>营销服务</div>
+            {
+                MARKETSERVICE_CONFIG.map((productItem,index) => (
+                    <div className='marketing-service-item'
+                        key={`index-menuItem${index.toString()}`}
+                        onClick={()=>{this.handleServiceItemClick(productItem.link)}}
+                    >
+                        <img src={productItem.marketToolImgUrl} alt="" />
+                        <div className='name'>{productItem.marketToolTitle }</div>
+                    </div>
+                ))
+            }
+      </div>
+    )
+  }
+}
